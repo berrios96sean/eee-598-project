e70781ea97d9b9657068ed99b9057659d2becbd4
itsruthwik <itsruthwik@outlook.com>
2024-11-25 20:56:09 -0700

    Update classifier module names

--- src/wrapper/ensemble_wrapper_2.v
+++ src/wrapper/ensemble_wrapper_2.v
@@ -1,14 +1,30 @@
 // gaussian_nb  logistic_regression  gradient_boost
 
-module ensemble_wrapper1 #(
+
+module ensemble_wrapper_2 #(
     parameter DATA_WIDTH = 32,
     parameter KEEP_WIDTH = 4
 )(
     input wire clk,
     input wire rst_n,
 
-    // Classifier 1
+    // Classifier 0
     // AXI-Stream input interface 
+    input wire [DATA_WIDTH-1:0] s_axis_tdata_0,
+    input wire [KEEP_WIDTH-1:0] s_axis_tkeep_0,
+    input wire                  s_axis_tvalid_0,
+    output wire                 s_axis_tready_0,
+    input wire                  s_axis_tlast_0,
+
+    // AXI-Stream output interface
+    output wire [DATA_WIDTH-1:0] m_axis_tdata_0,
+    output wire [KEEP_WIDTH-1:0] m_axis_tkeep_0,
+    output wire                  m_axis_tvalid_0,
+    input wire                   m_axis_tready_0,
+    output wire                  m_axis_tlast_0,
+
+    // Classifier 1
+    // AXI-Stream input interface
     input wire [DATA_WIDTH-1:0] s_axis_tdata_1,
     input wire [KEEP_WIDTH-1:0] s_axis_tkeep_1,
     input wire                  s_axis_tvalid_1,
@@ -35,25 +51,27 @@ module ensemble_wrapper1 #(
     output wire [KEEP_WIDTH-1:0] m_axis_tkeep_2,
     output wire                  m_axis_tvalid_2,
     input wire                   m_axis_tready_2,
-    output wire                  m_axis_tlast_2,
-
-    // Classifier 3
-    // AXI-Stream input interface
-    input wire [DATA_WIDTH-1:0] s_axis_tdata_3,
-    input wire [KEEP_WIDTH-1:0] s_axis_tkeep_3,
-    input wire                  s_axis_tvalid_3,
-    output wire                 s_axis_tready_3,
-    input wire                  s_axis_tlast_3,
-
-    // AXI-Stream output interface
-    output wire [DATA_WIDTH-1:0] m_axis_tdata_3,
-    output wire [KEEP_WIDTH-1:0] m_axis_tkeep_3,
-    output wire                  m_axis_tvalid_3,
-    input wire                   m_axis_tready_3,
-    output wire                  m_axis_tlast_3
+    output wire                  m_axis_tlast_2
 );
 
-    gaussian_nb gnb_0 (
+    gaussian_nb_0 gnb_0 (
+        .ap_clk(clk),
+        .ap_rst_n(rst_n),
+        .in_stream_TDATA(s_axis_tdata_0),
+        .in_stream_TVALID(s_axis_tvalid_0),
+        .in_stream_TREADY(s_axis_tready_0),
+        .in_stream_TKEEP(s_axis_tkeep_0),
+        .in_stream_TSTRB(2'b11),
+        .in_stream_TLAST(s_axis_tlast_0),
+        .out_stream_TDATA(m_axis_tdata_0),
+        .out_stream_TVALID(m_axis_tvalid_0),
+        .out_stream_TREADY(m_axis_tready_0),
+        .out_stream_TKEEP(m_axis_tkeep_0),
+        // .out_stream_TSTRB(2'b11),
+        .out_stream_TLAST(m_axis_tlast_0)
+    );
+
+    logistic_regression_0 lr_0 (
         .ap_clk(clk),
         .ap_rst_n(rst_n),
         .in_stream_TDATA(s_axis_tdata_1),
@@ -70,7 +88,7 @@ module ensemble_wrapper1 #(
         .out_stream_TLAST(m_axis_tlast_1)
     );
 
-    logistic_regression lr_0 (
+    gradient_boost_0 mlp_0 (
         .ap_clk(clk),
         .ap_rst_n(rst_n),
         .in_stream_TDATA(s_axis_tdata_2),
@@ -87,22 +105,4 @@ module ensemble_wrapper1 #(
         .out_stream_TLAST(m_axis_tlast_2)
     );
 
-    gradient_boost gb_0 (
-        .ap_clk(clk),
-        .ap_rst_n(rst_n),
-        .in_stream_TDATA(s_axis_tdata_3),
-        .in_stream_TVALID(s_axis_tvalid_3),
-        .in_stream_TREADY(s_axis_tready_3),
-        .in_stream_TKEEP(s_axis_tkeep_3),
-        .in_stream_TSTRB(2'b11),
-        .in_stream_TLAST(s_axis_tlast_3),
-        .out_stream_TDATA(m_axis_tdata_3),
-        .out_stream_TVALID(m_axis_tvalid_3),
-        .out_stream_TREADY(m_axis_tready_3),
-        .out_stream_TKEEP(m_axis_tkeep_3),
-        // .out_stream_TSTRB(2'b11),
-        .out_stream_TLAST(m_axis_tlast_3)
-    );
-
 endmodule
-
--- src/wrapper/ensemble_wrapper_black_box.v
+++ src/wrapper/ensemble_wrapper_black_box.v
@@ -1,12 +1,28 @@
-module ensemble_wrapper1 #(
+
+module ensemble_wrapper_black_box #(
     parameter DATA_WIDTH = 32,
     parameter KEEP_WIDTH = 4
 )(
     input wire clk,
     input wire rst_n,
 
-    // Classifier 1
+    // Classifier 0
     // AXI-Stream input interface 
+    input wire [DATA_WIDTH-1:0] s_axis_tdata_0,
+    input wire [KEEP_WIDTH-1:0] s_axis_tkeep_0,
+    input wire                  s_axis_tvalid_0,
+    output wire                 s_axis_tready_0,
+    input wire                  s_axis_tlast_0,
+
+    // AXI-Stream output interface
+    output wire [DATA_WIDTH-1:0] m_axis_tdata_0,
+    output wire [KEEP_WIDTH-1:0] m_axis_tkeep_0,
+    output wire                  m_axis_tvalid_0,
+    input wire                   m_axis_tready_0,
+    output wire                  m_axis_tlast_0,
+
+    // Classifier 1
+    // AXI-Stream input interface
     input wire [DATA_WIDTH-1:0] s_axis_tdata_1,
     input wire [KEEP_WIDTH-1:0] s_axis_tkeep_1,
     input wire                  s_axis_tvalid_1,
@@ -33,44 +49,28 @@ module ensemble_wrapper1 #(
     output wire [KEEP_WIDTH-1:0] m_axis_tkeep_2,
     output wire                  m_axis_tvalid_2,
     input wire                   m_axis_tready_2,
-    output wire                  m_axis_tlast_2,
-
-    // Classifier 3
-    // AXI-Stream input interface
-    input wire [DATA_WIDTH-1:0] s_axis_tdata_3,
-    input wire [KEEP_WIDTH-1:0] s_axis_tkeep_3,
-    input wire                  s_axis_tvalid_3,
-    output wire                 s_axis_tready_3,
-    input wire                  s_axis_tlast_3,
-
-    // AXI-Stream output interface
-    output wire [DATA_WIDTH-1:0] m_axis_tdata_3,
-    output wire [KEEP_WIDTH-1:0] m_axis_tkeep_3,
-    output wire                  m_axis_tvalid_3,
-    input wire                   m_axis_tready_3,
-    output wire                  m_axis_tlast_3
+    output wire                  m_axis_tlast_2
 );
 
-// Assign outputs for Classifier 1
+// Classifier 0
+assign m_axis_tdata_0 = s_axis_tdata_0;
+assign m_axis_tkeep_0 = s_axis_tkeep_0;
+assign m_axis_tvalid_0 = s_axis_tvalid_0;
+assign s_axis_tready_0 = m_axis_tready_0;
+assign m_axis_tlast_0 = s_axis_tlast_0;
+
+// Classifier 1
 assign m_axis_tdata_1 = s_axis_tdata_1;
 assign m_axis_tkeep_1 = s_axis_tkeep_1;
 assign m_axis_tvalid_1 = s_axis_tvalid_1;
 assign s_axis_tready_1 = m_axis_tready_1;
 assign m_axis_tlast_1 = s_axis_tlast_1;
 
-// Assign outputs for Classifier 2
+// Classifier 2
 assign m_axis_tdata_2 = s_axis_tdata_2;
 assign m_axis_tkeep_2 = s_axis_tkeep_2;
 assign m_axis_tvalid_2 = s_axis_tvalid_2;
 assign s_axis_tready_2 = m_axis_tready_2;
 assign m_axis_tlast_2 = s_axis_tlast_2;
 
-// Assign outputs for Classifier 3
-assign m_axis_tdata_3 = s_axis_tdata_3;
-assign m_axis_tkeep_3 = s_axis_tkeep_3;
-assign m_axis_tvalid_3 = s_axis_tvalid_3;
-assign s_axis_tready_3 = m_axis_tready_3;
-assign m_axis_tlast_3 = s_axis_tlast_3;
-    
 endmodule
-
